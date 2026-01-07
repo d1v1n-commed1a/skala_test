facd60a970ab740259528d4a43e75db340e377f6
방다원 <dawon9203@outlook.kr>
2026-01-07 13:00:55 +0900

    change1

---
+++ ehco.py
@@ -0,0 +1,8 @@
+# 사용자가 입력한 말을 그대로 출력하는 프로그램
+
+def main():
+    user_input = input("문장을 입력하세요: ")  # 사용자 입력 받기
+    print("입력하신 문장은:", user_input)      # 입력받은 문장 출력
+
+if __name__ == "__main__":
+    main()
