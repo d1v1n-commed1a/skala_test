201bcf3bc309d0a2660efc7765bf0526d69943a1
방다원 <dawon9203@outlook.kr>
2026-01-07 13:05:51 +0900

    sc2

--- ehco.py
+++ ehco.py
@@ -1,7 +1,7 @@
 # 사용자가 입력한 말을 그대로 출력하는 프로그램
 
 def main():
-    user_input = input("문장을 입력하세요: ")  # 사용자 입력 받기
+    user_input = input("문장을 입력하세요: ")  # 사용자 입력 받기!
     print("입력하신 문장은:", user_input)      # 입력받은 문장 출력
 
 if __name__ == "__main__":
